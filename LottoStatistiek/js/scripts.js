e34d9c82d5ba044f4f2acbffd2eed33c7ef9f343
Afrem <71547964+AfremDilmac@users.noreply.github.com>
2021-02-24 12:34:14 +0100

    commit

--- LottoStatistiek/js/scripts.js
+++ LottoStatistiek/js/scripts.js
@@ -3,106 +3,33 @@ const ZES_WILLEKEURIG = 6;
 const VIJFTIG_POGINGEN = 50;
 
 //6 willekeurige cijfers
-let arr1 = []
 
-arr1[1] = Math.floor(Math.random() * CIJFERS);
-arr1[2] = Math.floor(Math.random() * CIJFERS);
-arr1[3] = Math.floor(Math.random() * CIJFERS);
-arr1[4] = Math.floor(Math.random() * CIJFERS);
-arr1[5] = Math.floor(Math.random() * CIJFERS);
-
-
-for (let index = 0; index < ZES_WILLEKEURIG; index++) {
-    arr1.push (Math.floor(Math.random() * CIJFERS));
-    
-}
-console.log(arr1);
-//trekking
-// console.log("getrokken getallen: " + arr1.toString());
-
-let arr2 = []
-arr2[0] = Math.floor(Math.random() * CIJFERS);
-arr2[1] = Math.floor(Math.random() * CIJFERS);
-arr2[2] = Math.floor(Math.random() * CIJFERS);
-arr2[3] = Math.floor(Math.random() * CIJFERS);
-arr2[4] = Math.floor(Math.random() * CIJFERS);
-arr2[5] = Math.floor(Math.random() * CIJFERS);
-
-let count = 0;
-if (arr1[0] == arr2[0]) {
-    count++;
-}
-if (arr1[1] == arr2[1]) {
-    count++;
-}
-if (arr1[2] == arr2[2]) {
-    count++;
-}
-if (arr1[3] == arr2[3]) {
-    count++;
-}
-if (arr1[4] == arr2[4]) {
-    count++;
-}
-if (arr1[5] == arr2[5]) {
-    count++;
+function genereerUniekeGetallenreeks(aantal, max){
+    let getallen = [];
+    for (let index = 0; index < aantal; index++) {
+        
+        
+        if (getallen.includes(nieuwGetal)) {
+            continue;
+            getallen.push();
+        }
+    }
+    return getallen;
 }
 
-let arr3 = []
-arr3[0] = Math.floor(Math.random() * CIJFERS);
-arr3[1] = Math.floor(Math.random() * CIJFERS);
-arr3[2] = Math.floor(Math.random() * CIJFERS);
-arr3[3] = Math.floor(Math.random() * CIJFERS);
-arr3[4] = Math.floor(Math.random() * CIJFERS);
-arr3[5] = Math.floor(Math.random() * CIJFERS);
+let getrokkenGetallen = genereerUniekeGetallenreeks(ZES_WILLEKEURIG)
+console.log(getrokkenGetallen);
 
-let count1 = 0;
-if (arr1[0] == arr3[0]) {
-    count1++;
+function nummer(){
+    let nummers = [];
+    for (let index = 0; index < 6; index++) {
+        let num = Math.floor(Math.random() * 42) + 1;
+        for (let index = 0; index < 42; index++) {
+            if (num.includes(nummers)) {
+             return   
+            }
+            nummers.push(num);
+        }
+        
+    }
 }
-if (arr1[1] == arr3[1]) {
-    count1++;
-}
-if (arr1[2] == arr3[2]) {
-    count1++;
-}
-if (arr1[3] == arr3[3]) {
-    count1++;
-}
-if (arr1[4] == arr3[4]) {
-    count1++;
-}
-if (arr1[5] == arr3[5]) {
-    count1++;
-}
-
-let arr4 = []
-arr4[0] = Math.floor(Math.random() * CIJFERS);
-arr4[1] = Math.floor(Math.random() * CIJFERS);
-arr4[2] = Math.floor(Math.random() * CIJFERS);
-arr4[3] = Math.floor(Math.random() * CIJFERS);
-arr4[4] = Math.floor(Math.random() * CIJFERS);
-arr4[5] = Math.floor(Math.random() * CIJFERS);
-
-let count2 = 0;
-if (arr1[0] == arr4[0]) {
-    count2++;
-}
-if (arr1[1] == arr4[1]) {
-    count2++;
-}
-if (arr1[2] == arr4[2]) {
-    count2++;
-}
-if (arr1[3] == arr4[3]) {
-    count2++;
-}
-if (arr1[4] == arr4[4]) {
-    count2++;
-}
-if (arr1[5] == arr4[5]) {
-    count2++;
-}
-
-//resultaten 50 spelers
-console.log("aantal juist: " + count + "," + count1 + "," + count2);
