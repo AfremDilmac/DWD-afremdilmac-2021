855884b7b99b7c1611f5c77c742045359220bc64
Afrem <71547964+AfremDilmac@users.noreply.github.com>
2021-02-28 15:14:21 +0100

    commit

---
+++ 02.DWD - DOM deel 1/FormCheckingFull/js/scripts.js
@@ -0,0 +1,59 @@
+const frmLogin = document.querySelector('#frmLogin');
+const inpEmail = frmLogin.querySelector('#email');
+const inpName = frmLogin.querySelector('#name');
+const inpBirth = frmLogin.querySelector('#birth');
+const inpProfile = frmLogin.querySelector('#profile');
+const inpMan = frmLogin.querySelector('#rbnman');
+const inpVrouw = frmLogin.querySelector('#rbnvrouw');
+const inpInterests = frmLogin.querySelectorAll('#chbsInterests')
+const msgName = frmLogin.querySelector('#msgname');
+const msgEmail = frmLogin.querySelector('#msgemail');
+const msgBirth = frmLogin.querySelector('#msgbirth');
+const msgProfile = frmLogin.querySelector('#msgprofile');
+const msgGender = frmLogin.querySelector('#msggender');
+const msgInterests = frmLogin.querySelector('#msginterests')
+
+frmLogin
+.setAttribute
+('novalidate', 'novalidate');
+
+frmLogin.addEventListener('submit', function (e){
+    e.preventDefault();
+    let numErrors = 0;
+   
+    if (inpEmail.value == '') {
+        msgEmail.innerHTML = 'mag niet leeg zijn';
+        numErrors++;
+    }
+    else if (inpEmail.value != '') {
+        msgEmail.innerHTML = '';
+
+    }
+    if (inpName.value == '') {
+        msgName.innerHTML = 'mag niet leeg zijn';
+        numErrors++;
+    }
+    else if (inpName.value != '') {
+        msgName.innerHTML = '';
+    }
+    if (inpBirth.value == '') {
+        msgBirth.innerHTML = 'geboortedatum mag niet leeg zijn';
+        numErrors++;
+    }
+    else if (inpBirth.value != '') {
+        msgBirth.innerHTML = '';
+    }
+    if (inpProfile.selectedIndex == 0) {
+        msgProfile.innerHTML = 'Selecteer een profiel';
+    }
+    else if (inpProfile.selectedIndex != 0) {
+        msgProfile.innerHTML = '';
+    }
+    if (inpMan.checked == false && inpVrouw.checked == false) {
+        msgGender.innerHTML = 'Selecteer een geslacht';
+    }
+    else if (inpMan.checked == true || inpVrouw.checked == true) {
+        msgGender.innerHTML = '';
+    }
+    
+})
