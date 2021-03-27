0422709f72a916a5e348f6cf8c6ce37d65af3c2a
Afrem <71547964+AfremDilmac@users.noreply.github.com>
2021-03-27 15:53:16 +0100

    commit

---
+++ 04.DWD - Web API/ChuckNorrisQuotes/js/scripts.js
@@ -0,0 +1,26 @@
+const blockquote = document.querySelector('#blockquote');
+const img = document.querySelector('#image');
+
+    // fetch settings; PAS DIT AAN
+let url = 'https://api.chucknorris.io/jokes/random'; // basis url
+
+let options = {}; // extra opties
+let params = new URLSearchParams(); // extra parameters
+params.append('parameter1', 'value1');
+params.append('parameter2', 'value2');
+url += '?' + params.toString();
+// fetch url; LAAT ONGEWIJZIGD
+fetch(url, options)
+.then(resp => { return resp.json(); })
+.then(data => verwerkData(data))
+.catch(err => verwerkFout(err));
+// verwerk fouten – PAS DIT EVENTUEEL AAN
+function verwerkFout(err) {
+console.log('request mislukt: ', err)
+}
+// verwerk data – PAS DIT AAN
+function verwerkData(data) {
+blockquote.innerHTML = data.value;
+img.src = data.icon_url;
+console.log(data);
+}
---
+++ 04.DWD - Web API/OpenWeatherMap/js/scripts.js
@@ -0,0 +1,36 @@
+const lblWeather = document.querySelector('#weather');
+
+// fetch settings; PAS DIT AAN
+let url = 'https://api.openweathermap.org/data/2.5/weather?id=2800866&appid=f872ea812be596f5bcdbd8ba36387f8b&units=metric&lang=nl'; // basis url
+let options = {}; // extra opties
+let params = new URLSearchParams(); // extra parameters
+params.append('q', 'appid');
+params.append('lang', 'units');
+
+
+// fetch url; LAAT ONGEWIJZIGD
+fetch(url, options)
+.then(resp => { return resp.json(); })
+.then(data => verwerkData(data))
+.catch(err => verwerkFout(err));
+// verwerk fouten – PAS DIT EVENTUEEL AAN
+function verwerkFout(err) {
+console.log('request mislukt: ', err)
+}
+// verwerk data – PAS DIT AAN
+function verwerkData(data) {
+console.log('ontvangen data: ', data)
+lblWeather.innerHTML = `It is in ${data.name} ${data.main.temp} Celsius and with a wind with ${data.wind.speed} speed`;
+}
+
+/*{
+    "id": 2800866,
+    "name": "Brussels",
+    "state": "",
+    "country": "BE",
+    "coord": {
+        "lon": 4.34878,
+        "lat": 50.850449
+    }
+
+*/
