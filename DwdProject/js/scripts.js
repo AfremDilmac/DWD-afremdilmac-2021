258371f6363e979f0460d2d2cd92a951ab746874
Afrem <71547964+AfremDilmac@users.noreply.github.com>
2021-04-10 19:27:31 +0200

    commit

--- DwdProject/js/scripts.js
+++ DwdProject/js/scripts.js
@@ -12,125 +12,129 @@ const countdown = document.querySelector('#countdown');
 const buttons = document.querySelectorAll('.options button');
 
 //Start game
-btnPlay.addEventListener('click', function(){
+btnPlay.addEventListener('click', function () {
     sndStart.play();
 });
-
-//Canvas
-canvas.width = 800;
-canvas.height = 500;
-//Character movement geleerd op bron: https://www.youtube.com/watch?v=EYf_JwzwTlQ&t=923s
-const keys = [];
-
-//character parameters
-const player = {
-    x: 400,
-    y: 220,
-    width: 32,
-    height: 48,
-    frameX: 0,
-    frameY: 0,
-    speed: 1,
-    moving: false,
-};
-
-const playerSprite = new Image();
-playerSprite.src = "img/sprite.png";
-
-function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
-    ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
-}
-//Character animation
-function animate(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
-    movePlayer();
-    requestAnimationFrame(animate);
-    handlePlayerFrame();
-
-}
-animate();
-
-//Animation On/Off wanneer ik op een toets druk of niet.
-function handlePlayerFrame(){
-    if (player.frameX < 3 && player.moving) {
-        player.frameX++;
-    }
-    else player.frameX = 0;
-}
-
-window.addEventListener("keydown", function(e){
-    keys[e.keyCode] = true;
-    player.moving = true;
-});
-
-window.addEventListener("keyup", function(e){
-    delete keys[e.keyCode];
-    player.moving = false;
-});
-
-//character movement
-function movePlayer(){
-    if (keys[38] && player.y > 0 ) {
-        player.y -= player.speed;
-        player.frameY = 3;
-    }
-
-    if (keys[37] && player.x > 20 ) {
-        player.x -= player.speed;
-        player.frameY = 1;
-    }
-
-    if (keys[40] && player.y < canvas.height - 120) {
-        player.y += player.speed;
-        player.frameY = 0;
-    }
-
-    if (keys[39] && player.x < canvas.width - 52) {
-        player.x += player.speed;
-        player.frameY = 2;
-    }
-}
-
-
-
-//Sound
-let sndStart = new Audio();
-sndStart.volume = 0.5;
-sndStart.src = "snd/play.mp3";
-
-let sndOptions = new Audio();
-sndOptions.volume = 0.5;
-sndOptions.src = "snd/options.mp3"
-
-sldSize.addEventListener('click', function () {
-    txtSound.innerHTML = sldSize.value + '%';
-    let sound = sldSize.value / 100;
-    sndStart.volume = sound;
-});
-
-
-
-//Options + sound foreach button
-buttons.forEach(btn => {
-    btn.addEventListener('click', function () {
-        document.querySelector('.current').classList.remove('current');
-        btn.classList.add('current');
-        sndOptions.play();
-    });
-});
-btnFullScreen.addEventListener('click', function(){
+btnPlay.addEventListener('click', function (e) {
     document.getElementById("canvas1").requestFullscreen()
-});
 
-btnMuteSound.addEventListener('click', function(){
-    sndStart.muted = true;
-});
+    //Canvas
+    canvas.width = 800;
+    canvas.height = 500;
+    //Character movement geleerd op bron: https://www.youtube.com/watch?v=EYf_JwzwTlQ&t=923s
+    const keys = [];
 
-btnUnmuteSound.addEventListener('click', function(){
-    sndStart.muted = false;
-});
+    //character parameters
+    const player = {
+        x: 400,
+        y: 220,
+        width: 32,
+        height: 48,
+        frameX: 0,
+        frameY: 0,
+        speed: 1,
+        moving: false,
+    };
 
-btnNormalScreen.addEventListener('click', function(){
-    document.exitFullscreen();
+    const playerSprite = new Image();
+    playerSprite.src = "img/sprite.png";
+
+    function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
+        ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
+    }
+    //Character animation
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
+        movePlayer();
+        requestAnimationFrame(animate);
+        handlePlayerFrame();
+
+    }
+    animate();
+
+    //Animation On/Off wanneer ik op een toets druk of niet.
+    function handlePlayerFrame() {
+        if (player.frameX < 3 && player.moving) {
+            player.frameX++;
+        }
+        else player.frameX = 0;
+    }
+
+    window.addEventListener("keydown", function (e) {
+        keys[e.keyCode] = true;
+        player.moving = true;
+    });
+
+    window.addEventListener("keyup", function (e) {
+        delete keys[e.keyCode];
+        player.moving = false;
+    });
+
+    //character movement
+    function movePlayer() {
+        if (keys[38] && player.y > 0) {
+            player.y -= player.speed;
+            player.frameY = 3;
+        }
+
+        if (keys[37] && player.x > 20) {
+            player.x -= player.speed;
+            player.frameY = 1;
+        }
+
+        if (keys[40] && player.y < canvas.height - 120) {
+            player.y += player.speed;
+            player.frameY = 0;
+        }
+
+        if (keys[39] && player.x < canvas.width - 52) {
+            player.x += player.speed;
+            player.frameY = 2;
+        }
+    }
+
+
+
+    //Sound
+    let sndStart = new Audio();
+    sndStart.volume = 0.5;
+    sndStart.src = "snd/play.mp3";
+
+    let sndOptions = new Audio();
+    sndOptions.volume = 0.5;
+    sndOptions.src = "snd/options.mp3"
+
+    sldSize.addEventListener('click', function () {
+        txtSound.innerHTML = sldSize.value + '%';
+        let sound = sldSize.value / 100;
+        sndStart.volume = sound;
+    });
+
+
+
+    //Options + sound foreach button
+    buttons.forEach(btn => {
+        btn.addEventListener('click', function () {
+            document.querySelector('.current').classList.remove('current');
+            btn.classList.add('current');
+            sndOptions.play();
+        });
+    });
+
+    btnMuteSound.addEventListener('click', function () {
+        sndStart.muted = true;
+    });
+
+    btnUnmuteSound.addEventListener('click', function () {
+        sndStart.muted = false;
+    });
+
+    btnNormalScreen.addEventListener('click', function () {
+        document.exitFullscreen();
+    });
+
+    btnFullScreen.addEventListener('click', function () {
+        document.getElementById("canvas1").requestFullscreen()
+    });
 });
